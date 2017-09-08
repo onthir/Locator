6253f4c5dd280e69337d768b872e0a6ea64d9bdd
onthir <jny.bbk1@gmail.com>
2017-09-08 21:06:18 +0545

    Update locator.py

--- locator.py
+++ locator.py
@@ -4,14 +4,15 @@ import urllib.parse
 
 root = Tk()
 root.title("Complete Address Finder")
-root.geometry('1920x1080+50+50')
-heading = Label(root, text="Complete Address Finder", font=('arial 20 bold'), fg="steelblue").pack()
+root.geometry('1920x1080')
+root.configure(background="orange")
+heading = Label(root, text="Address Locator", font=('arial 50 bold'), fg="steelblue", bg="orange").pack()
 
-lab = Label(root, text="Enter Location, Schools, Restaurants....etc.", font=('arial 13 bold')).pack()
+lab = Label(root, text="Enter Location, Schools, Restaurants....etc.", font=('arial 25 bold'), bg="orange").place(x=400, y=200)
 
 
 text = StringVar()
-ent = Entry(root, width=50, textvariable=text).pack()
+ent = Entry(root, width=50, textvariable=text).place(x=400, y=250)
 
 def  main_function():
      
@@ -27,24 +28,10 @@ def  main_function():
      if json_status == 'OK':
           
           formatted_address = json_data['results'][0]['formatted_address']
-          label2 = Label(root, text=formatted_address, font=('arial 15 bold'), fg="green", bg="lightgrey", width=80).place(x=100, y=160)
+          label2 = Label(root, text=formatted_address, font=('arial 15 bold'), fg="green", bg="lightblue", width=80).place(x=400, y=400)
      else:
-          labell = Label(root, text="Invalid Request", font=('arial 13 bold'), fg="red").pack()
+          labell = Label(root, text="Invalid Request", font=('arial 15 bold'), fg="red", bg="lightblue").place(x = 400, y= 450)
 
-btn = Button(root, text="Find", width=30, height=3, bg="lightgreen", command=main_function).pack()
+btn = Button(root, text="Find", width=30, height=3, fg="white", bg="black", command=main_function).place(x=400, y=300)
 
 root.mainloop()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
